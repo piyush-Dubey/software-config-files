1d309b57213fb9eec3877bd765488a0f0929d8c5
Piyush Dubey <piyushdubey1996@gmail.com>
2021-01-23 00:55:21 +0530

    Move the contents of .emacs to .emacs.d/init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,6 +3,34 @@
 ;; -*- emacs-lisp -*-
 
 ;;; Code:
+
+(require 'package)
+;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+
+(package-initialize)
+
+(package-install 'flycheck)
+(global-flycheck-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (rust-mode flycheck-rust htmlize flycheck-clang-tidy flycheck))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (eval-after-load 'flycheck
   '(add-hook 'flycheck-mode-hook #'flycheck-clang-tidy-setup))
 
@@ -10,6 +38,9 @@
   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages '((C . t)))
 
 (provide 'init)
 ;;; init.el ends here
